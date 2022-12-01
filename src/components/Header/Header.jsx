fc6a00ab0b4f7b02b9aee20109860248d1d65983
Alex <Alex031994@gmail.com>
2022-12-01 18:13:52 +0300

    changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <header className='header'>
-            header
+            <img src="" alt="logo"/>
+            <div className="location">Москва и МО</div>
         </header>
     );
 };
